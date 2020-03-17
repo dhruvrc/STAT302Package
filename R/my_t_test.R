3324208c56d620fec6536324040887e49c6b6cf1
Dhruv Chintalapati <dhruvrc@gmail.com>
2020-03-16 20:38:37 -0700

    Adding my_t.test function

---
+++ R/my_t_test.R
@@ -0,0 +1,25 @@
+#' T-test function
+#'
+#' This function performs a one sample t-test in R.
+#'
+#' @param x Numeric vector of data.
+#' @param alternative Character string specifying the alternative hypothesis. This should only accept `
+#' "two.sided"`, `"less"`, or `"greater".
+#' @param mu Number indicating the null hypothesis value of the mean.
+#'
+#' @return A list containing the numeric test statistic, degrees of freedom, value of the \code{alternative}
+#' parameter, and the numeric p-value.
+#'
+#' @export
+my_t.test <- function(x, alternative, mu) {
+  if (alternative != "two-sided" && alternative != "less" && alternative != "greater") {
+    stop("alternative needs to be input as 'two-sided', 'less', or 'greater'")
+  }
+  sample_size <- length(x)
+  df <- sample_size - 1
+  standard_error <- sd(x) / sqrt(sample_size)
+  sample_mean <- mean(x)
+  test_stat <- (mean(x) - mu) / standard_error
+  results <- list("test_stat" = test_stat, "df" = df, "alternative" = alternative, "p_val")
+  return(results)
+}
